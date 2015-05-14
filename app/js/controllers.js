67da997d22c731e7dfb00409ffd46b5f2c95e51d
Aleksander <grkalk@gmail.com>
2015-05-14 14:11:28 +0200

    dzialajacy test

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,7 +5,16 @@
 var webMathEditorControllers = angular.module('webMathEditorControllers', []);
 
 webMathEditorControllers.controller('MathEditor', ['$scope', function($scope) {
+
+    $scope.mathExp = "x = {-b \\pm \\sqrt{b^2-4ac} \\over 2a}";
+
     $scope.latexValue = function(latexVal) {
-        setLatexExpression(latexVal)
-    }
+        setLatexExpression(latexVal);
+        Preview.Update();
+    };
+
+    $scope.updatePreview = function() {
+        Preview.Update();
+    };
+
 }]);
--- test/unit/mathEditorControllerSpec.js
+++ test/unit/mathEditorControllerSpec.js
@@ -3,21 +3,13 @@
 describe('MathEditor controller', function() {
 
     beforeEach(module('webMathEditor'));
-    beforeEach(module('webMathEditorControllers'));
 
-    var $controller;
-
-    beforeEach(inject(function(_$controller_) {
-        $controller = _$controller_;
-    }));
-
-    //TODO: Test do poprawy
-    it('checks if latex expression changed after update', function() {
+    it('checks if latex expression changed after calling update function', function($controller) {
         var $scope = {};
         var controller = $controller('MathEditor', { $scope: $scope });
         var latexVal = '(a+b)';
         var latexExpress = $('#mathExpression');
         $scope.latexValue(latexVal);
-        expect(latexVal).toContain(latexVal);
+        expect(latexExpress.text()).toBe(latexVal);
     });
 });
